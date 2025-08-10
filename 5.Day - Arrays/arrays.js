25c445edfcbc6e0dbd6204c21c1bc0f84c116505
ImrenR <imrenrahbay@gmail.com>
2025-08-10 21:10:15 +0100

    Exercise 3 done

--- 5.Day - Arrays/arrays.js
+++ 5.Day - Arrays/arrays.js
@@ -258,7 +258,7 @@ const backEnd = ['Node','Express', 'MongoDB']
 const fullStack1 = [...frontEnd, ...backEnd];
 console.log(fullStack1);
 
-console.log(fullStack)
+
 
 // ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"]
 
@@ -267,14 +267,27 @@ console.log(fullStack)
 //***************************** EXERCISES - 3 ************************* */
 
 //? 1- The following is an array of 10 students ages:
+
 const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
 
 // Sort the array and find the min and max age
+
+const arrange = ages.sort((a,b)=> a-b);
+console.log(ages);
+
+const minAge = ages[0];
+console.log(minAge);
+
+const maxAge = ages[ages.length-1]
+console.log(maxAge)
+
 // Find the median age(one middle item or two middle items divided by two)
+
+const medianAge = ages[ages.length/2 - 1];
+console.log(medianAge);
+
 // Find the average age(all items divided by number of items)
-// Find the range of the ages(max minus min)
-// Compare the value of (min - average) and (max - average), use abs() method 1.Slice the first ten countries from the countries array
+const sum1 = ages.reduce((acc,curr)=> acc + curr, 0);
+const averageAge = sum1 / ages.length;
+console.log(averageAge);
 
-//? 2- Find the middle country(ies) in the countries array
-
-//? 3- Divide the countries array into two equal arrays if it is even. If countries array is not even , one more country for the first half.
