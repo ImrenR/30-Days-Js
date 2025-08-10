e1283b0f703e7c3054fd4ab74c13901a6f20f90c
ImrenR <imrenrahbay@gmail.com>
2025-08-10 21:00:40 +0100

    Exercise-2 done

--- 5.Day - Arrays/arrays.js
+++ 5.Day - Arrays/arrays.js
@@ -161,64 +161,108 @@ console.log(removeAllCompanies);
 
 //***************************** EXERCISES - 2 ************************* */
 
-const countries = [
-  "Albania",
-  "Bolivia",
-  "Canada",
-  "Denmark",
-  "Ethiopia",
-  "Finland",
-  "Germany",
-  "Hungary",
-  "Ireland",
-  "Japan",
-  "Kenya",
-];
-
-const webTechs = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "React",
-  "Redux",
-  "Node",
-  "MongoDB",
-];
-
-//? 1- Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
-
-//? 2- First remove all the punctuations and change the string to array and count the number of words in the array
-
-// let text =
-// 'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
-// console.log(words)
-// console.log(words.length)
-
-// ["I", "love", "teaching", "and", "empowering", "people", "I", "teach", "HTML", "CSS", "JS", "React", "Python"]
-
-// 13
 
 //? 3- In the following shopping cart add, remove, edit items
 
-// const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
 
 // add 'Meat' in the beginning of your shopping cart if it has not been already added
+
+const addMeat = shoppingCart.push('Meat');
+console.log(shoppingCart); // added
+
+
 // add Sugar at the end of you shopping cart if it has not been already added
+
+const addSugar = shoppingCart.push("Sugar");
+console.log(shoppingCart);
+
 // remove 'Honey' if you are allergic to honey
+
+const honeyIndex = shoppingCart.indexOf('Honey');
+
+if(honeyIndex !== -1){
+  shoppingCart.splice(honeyIndex,1);
+}
+console.log(shoppingCart);
+
 // modify Tea to 'Green Tea'
 
-//? 4-In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
+const modifiedList = shoppingCart.indexOf("Tea");
 
-//? 5-In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+if(modifiedList !== -1){
+  shoppingCart[modifiedList] = 'Green Tea';
+}
+
+console.log(shoppingCart);
+
+//? 4-In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'.
+//? If it does not exist add to the countries list.
+
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya'
+]
+
+
+
+const addCountry = countries.indexOf('Ethiopia');
+if (addCountry !== -1){
+  console.log('Etiopia is existed in the list')
+}else {
+  countries.push('Ethiopia')
+}
+
+
+
+//? 5-In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'.
+//? If it does not exist add Sass to the array and print the array.
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+]
+
+const isExisted = webTechs.indexOf("Sass");
+
+if(isExisted !== -1){
+  console.log("Sass is a Css preprocess");
+}else
+{
+  webTechs.push('Sass')
+ 
+};
+
+console.log(webTechs);
 
 //? 6 -Concatenate the following two variables and store it in a fullStack variable.
 
-// const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
-// const backEnd = ['Node','Express', 'MongoDB']
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node','Express', 'MongoDB']
 
-// console.log(fullStack)
+
+const fullStack1 = [...frontEnd, ...backEnd];
+console.log(fullStack1);
+
+console.log(fullStack)
 
 // ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"]
+
+
 
 //***************************** EXERCISES - 3 ************************* */
 
