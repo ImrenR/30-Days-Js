ea9437a53bc892a901cbac8a95f49fa927284de1
ImrenR <imrenrahbay@gmail.com>
2025-08-10 15:34:32 +0100

    2. Day Done

--- 2.Day- Operators/boolean.js
+++ 2.Day- Operators/boolean.js
@@ -81,73 +81,94 @@ console.log(minute);
 const miliseconds = Date.now();
 const second = Math.floor(miliseconds / 1000 );
 
-console.log(seconds);
+console.log(second);
 
 //** EXERCISES -2 **/
 
 //***********************************************************************************/
 //? 1- Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
 
-let vase = 20;
+let base = 20;
 let height = 10;
 let tringle = 100;
 
+let area = 0.5 * base *height;
+
+console.log(`The area of the tringle is ${area}`);
+
 //? 2- Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c)
 
-//Enter side a: 5
-//Enter side b: 4
-//Enter side c: 3
-//The perimeter of the triangle is 12
+let  a =5;
+let  b = 4;
+let c = 3;
 
-//? 3 -Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+let triangle1 = a + b + c;
+console.log(triangle1);
 
-//? 4- Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle(c = 2 x pi x r) where pi = 3.14.
 
-//? 5-Calculate the slope, x-intercept and y-intercept of y = 2x -2
 
-//? 6-Slope is m = (y2-y1)/(x2-x1). Find the slope between point (2, 2) and point(6,10)
+//? 3-Write a script that prompt a user to enter hours and rate per hour. Calculate pay of the person?
 
-//? 7- Compare the slope of above two questions.
+ // let hours = prompt('Enter the hour');
+ // let rate = prompt('Enter the rate');
 
-//? 8-Calculate the value of y (y = x2 + 6x + 9). Try to use different x values and figure out at what x value y is 0.
+ // let earning = hours * rate;
 
-//? 9-Write a script that prompt a user to enter hours and rate per hour. Calculate pay of the person?
+ // console.log(earning); asnwer 1120
 
-//Enter hours: 40
-//Enter rate per hour: 28
-//Your weekly earning is 1120
-//? 10-If the length of your name is greater than 7 say, your name is long else say your name is short.
+//? 4-If the length of your name is greater than 7 say, your name is long else say your name is short.
 
-//? 11-Compare your first name length and your family name length and you should get this output.
+const yourName = 'Imrencim';
+
+if (yourName.length > 7 ) {
+  console.log(' Your name is long')
+}else {
+  console.log('Yourname is short')
+}
+
+//? 5-Compare your first name length and your family name length and you should get this output.
 
 let firstName = "Asabeneh";
 let lastName = "Yetayeh";
-// Your first name, Asabeneh is longer than your family name, Yetayeh
-//? 12-Declare two variables myAge and yourAge and assign them initial values and myAge and yourAge.
+
+if ( firstName.length > lastName.length) {
+  console.log(`Your first name, ${firstName} is longer than your family name, ${lastName}`)
+}else {
+  console.log('Your last name is longer than your first name')
+}
+
+//? 6-Declare two variables myAge and yourAge and assign them initial values and myAge and yourAge.
 
 let myAge = 250;
 let yourAge = 25;
-//I am 225 years older than you.
-//? 13-Using prompt get the year the user was born and if the user is 18 or above allow the user to drive if not tell the user to wait a certain amount of years.
 
-//Enter birth year: 1995
+let difference = myAge - yourAge;
+console.log(`I am ${difference} years older than you`);
+
+//? 7-Using prompt get the year the user was born and if the user is 18 or above allow the user to drive if not tell the user to wait a certain amount of years.
 //You are 25. You are old enough to drive
-
 //Enter birth year: 2005
 //You are 15. You will be allowed to drive after 3 years.
+//let birthYear = parseInt(prompt('Enter your birth year:'));
+//let currentYear = new Date().getFullYear();
+//let years = currentYear - birthYear;
+// if (years > 18){
+//   console.log(`You are ${years}. You are old enough to drive`)
+// }else {
+//   console.log('You are not allowed')
+// };
+//
 
-//? 14-Write a script that prompt the user to enter number of years. Calculate the number of seconds a person can live. Assume some one lives just hundred years
+//? 8-Write a script that prompt the user to enter number of years. Calculate the number of seconds a person can live. Assume some one lives just hundred years
 
 //Enter number of years you live: 100
+// let yearsLived = parseInt(prompt("Enter the years you lived:"));
+
+// let secondsLived = yearsLived * 365 * 24 * 60 * 60 ;
+
+// console.log(`You have lived approximately ${secondsLived} seconds.`);
 //You lived 3153600000 seconds.
 
-//? 15-Create a human readable time format using the Date time object
 
-//YYYY-MM-DD HH:mm
-//DD-MM-YYYY HH:mm
-//DD/MM/YYYY HH:mm
-//** EXERCISES -3 **/
 
-//***********************************************************************************/
-//? 1-Create a human readable time format using the Date time object. The hour and the minute should be all the time two digits(7 hours should be 07 and 5 minutes should be 05 )
-//YYY-MM-DD HH:mm eg. 20120-01-02 07:05
+
