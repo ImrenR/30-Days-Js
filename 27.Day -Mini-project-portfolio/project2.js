309caa0bb53b6be4f33baf148987826f3125d357
ImrenR <imrenrahbay@gmail.com>
2025-08-12 21:22:47 +0100

    update

---
+++ 27.Day -Mini-project-portfolio/project2.js
@@ -0,0 +1,2 @@
+//Create the following using HTML, CSS, and JavaScript
+//https://github.com/Asabeneh/30-Days-Of-JavaScript/blob/master/27_Day_Mini_project_portfolio/27_day_mini_project_portfolio.md
---
+++ 28.Day - Mini-Project/project3.js
@@ -0,0 +1,3 @@
+//Create the following using HTML, CSS, and JavaScript
+
+//https://github.com/Asabeneh/30-Days-Of-JavaScript/blob/master/28_Day_Mini_project_leaderboard/28_day_mini_project_leaderboard.md
