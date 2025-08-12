4b44204b40ebc54313d854db82046258d4c2140b
ImrenR <imrenrahbay@gmail.com>
2025-08-12 21:08:26 +0100

    update

---
+++ 18.Day - Promises/promises.js
@@ -0,0 +1,11 @@
+//*****************************  Exercises: Level 2  *******************************************************/
+
+//******************************************************************************************************** */
+const countriesAPI = 'https://restcountries.com/v2/all'
+const catsAPI = 'https://api.thecatapi.com/v1/breeds'
+
+
+//? 1-Print out all the cat names in to catNames variable.
+//? 2-Read the countries API using fetch and print the name of country, capital, languages, population and area.
+//?3-Read the cats api and find the average weight of cat in metric unit.
+//? 4-Read the countries api and find out the 10 largest countries
