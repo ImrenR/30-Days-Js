6bf83b4a0ed203e0c1f5ebd031621d8bf28079df
ImrenR <imrenrahbay@gmail.com>
2025-08-12 21:06:00 +0100

    update

---
+++ 17. Day - Web-Storages/storages.js
@@ -0,0 +1,6 @@
+//*****************************  Exercises: Level 2  *******************************************************/
+
+//******************************************************************************************************** */
+
+//? 1-Create a student object. The student object will have first name, last name, age, skills, country, enrolled keys and values for the keys. Store the student object in your browser localStorage.
+//? 2-Store you first name, last name, age, country, city in your browser localStorage.
