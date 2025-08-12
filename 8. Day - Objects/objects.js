69087c20b86ba6321f1b090bbc81f980dd8fbf02
ImrenR <imrenrahbay@gmail.com>
2025-08-12 17:58:57 +0100

    update

--- 8. Day - Objects/objects.js
+++ 8. Day - Objects/objects.js
@@ -2,12 +2,6 @@
 //*****************************  Exercises: Level 1  *******************************************************/
 
 //******************************************************************************************************** */
-
-
-//? 1-Find the person who has many skills in the users object.
-
-//? 2-Count logged in users, count users having greater than equal to 50 points from the following object.
-
 const users = {
   Alex: {
     email: 'alex@alex.com',
@@ -59,97 +53,147 @@ const users = {
     points: 40
   }
 }
+//******************************************************************************************************** */
+
+//? 1-Find the person who has many skills in the users object.
+//******************************************************************************************************** */
+ 
+let mostSkilledPerson = '';
+let maxSkills = 0;
+
+for (const user in users) {
+  const skillCount = users[user].skills.length;
+  if (skillCount>maxSkills){
+    maxSkills = skillCount;
+    mostSkilledPerson = user;
+  }
+}
+console.log(`The most skilled person is ${mostSkilledPerson} with ${maxSkills} skills`);
+
+//******************************************************************************************************** */
+//? 2-Count logged in users, count users having greater than equal to 50 points from the following object.
+//******************************************************************************************************** */
+
+//******************************************************************************************************** */
 //? 3-Find people who are MERN stack developer from the users object
+//******************************************************************************************************** */
 
+
+//******************************************************************************************************** */
 //? 4-Set your name in the users object without modifying the original users object
+//******************************************************************************************************** */
 
+
+
+//******************************************************************************************************** */
 //? 5-Get all keys or properties of users object
+//******************************************************************************************************** */
 
+//******************************************************************************************************** */
 //? 6-Get all the values of users object
+//******************************************************************************************************** */
 
+
+//******************************************************************************************************** */
 //? 7-Use the countries object to print a country name, capital, populations and languages.
+//******************************************************************************************************** */
 
 
 //*****************************  Exercises: Level 2  *******************************************************/
 
 //******************************************************************************************************** */Exercises: Level 3
 //? 1-Create an object literal called personAccount. It has firstName, lastName, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is a set of incomes and its description.
-//? 2-**** Questions:2, 3 and 4 are based on the following two arrays:users and products ()
-    const users = [
-    {
-        _id: 'ab12ex',
-        username: 'Alex',
-        email: 'alex@alex.com',
-        password: '123123',
-        createdAt:'08/01/2020 9:00 AM',
-        isLoggedIn: false
-    },
-    {
-        _id: 'fg12cy',
-        username: 'Asab',
-        email: 'asab@asab.com',
-        password: '123456',
-        createdAt:'08/01/2020 9:30 AM',
-        isLoggedIn: true
-    },
-    {
-        _id: 'zwf8md',
-        username: 'Brook',
-        email: 'brook@brook.com',
-        password: '123111',
-        createdAt:'08/01/2020 9:45 AM',
-        isLoggedIn: true
-    },
-    {
-        _id: 'eefamr',
-        username: 'Martha',
-        email: 'martha@martha.com',
-        password: '123222',
-        createdAt:'08/01/2020 9:50 AM',
-        isLoggedIn: false
-    },
-    {
-        _id: 'ghderc',
-        username: 'Thomas',
-        email: 'thomas@thomas.com',
-        password: '123333',
-        createdAt:'08/01/2020 10:00 AM',
-        isLoggedIn: false
-    }
-    ];
+//******************************************************************************************************** */
 
-    const products = [
-  {
-    _id: 'eedfcf',
-    name: 'mobile phone',
-    description: 'Huawei Honor',
-    price: 200,
-    ratings: [
-      { userId: 'fg12cy', rate: 5 },
-      { userId: 'zwf8md', rate: 4.5 }
-    ],
-    likes: []
-  },
-  {
-    _id: 'aegfal',
-    name: 'Laptop',
-    description: 'MacPro: System Darwin',
-    price: 2500,
-    ratings: [],
-    likes: ['fg12cy']
-  },
-  {
-    _id: 'hedfcg',
-    name: 'TV',
-    description: 'Smart TV:Procaster',
-    price: 400,
-    ratings: [{ userId: 'fg12cy', rate: 5 }],
-    likes: ['fg12cy']
-  }
-]
+
+
+
+
+
+
+//? 2-**** Questions:2, 3 and 4 are based on the following two arrays:users and products ()
+//     const users = [
+//     {
+//         _id: 'ab12ex',
+//         username: 'Alex',
+//         email: 'alex@alex.com',
+//         password: '123123',
+//         createdAt:'08/01/2020 9:00 AM',
+//         isLoggedIn: false
+//     },
+//     {
+//         _id: 'fg12cy',
+//         username: 'Asab',
+//         email: 'asab@asab.com',
+//         password: '123456',
+//         createdAt:'08/01/2020 9:30 AM',
+//         isLoggedIn: true
+//     },
+//     {
+//         _id: 'zwf8md',
+//         username: 'Brook',
+//         email: 'brook@brook.com',
+//         password: '123111',
+//         createdAt:'08/01/2020 9:45 AM',
+//         isLoggedIn: true
+//     },
+//     {
+//         _id: 'eefamr',
+//         username: 'Martha',
+//         email: 'martha@martha.com',
+//         password: '123222',
+//         createdAt:'08/01/2020 9:50 AM',
+//         isLoggedIn: false
+//     },
+//     {
+//         _id: 'ghderc',
+//         username: 'Thomas',
+//         email: 'thomas@thomas.com',
+//         password: '123333',
+//         createdAt:'08/01/2020 10:00 AM',
+//         isLoggedIn: false
+//     }
+//     ];
+
+//     const products = [
+//   {
+//     _id: 'eedfcf',
+//     name: 'mobile phone',
+//     description: 'Huawei Honor',
+//     price: 200,
+//     ratings: [
+//       { userId: 'fg12cy', rate: 5 },
+//       { userId: 'zwf8md', rate: 4.5 }
+//     ],
+//     likes: []
+//   },
+//   {
+//     _id: 'aegfal',
+//     name: 'Laptop',
+//     description: 'MacPro: System Darwin',
+//     price: 2500,
+//     ratings: [],
+//     likes: ['fg12cy']
+//   },
+//   {
+//     _id: 'hedfcg',
+//     name: 'TV',
+//     description: 'Smart TV:Procaster',
+//     price: 400,
+//     ratings: [{ userId: 'fg12cy', rate: 5 }],
+//     likes: ['fg12cy']
+//   }
+// ]
 // Imagine you are getting the above users collection from a MongoDB database. a. Create a function called signUp which allows user to add to the collection. If user exists, inform the user that he has already an account.
 // b. Create a function called signIn which allows user to sign in to the application
 
-//? 3-The products array has three elements and each of them has six properties. a. Create a function called rateProduct which rates the product b. Create a function called averageRating which calculate the average rating of a product
 
-//? 3-Create a function called likeProduct. This function will helps to like to the product if it is not liked and remove like if it was liked.
+//******************************************************************************************************** */
+//? 3-The products array has three elements and each of them has six properties. a. Create a function called rateProduct which rates the product b. Create a function called averageRating which calculate the average rating of a product
+//******************************************************************************************************** */
+
+
+
+
+//******************************************************************************************************** */
+//? 4-Create a function called likeProduct. This function will helps to like to the product if it is not liked and remove like if it was liked.
