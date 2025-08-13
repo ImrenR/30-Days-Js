b508d32fb93904370e69af4a6568bb55674f3f7b
ImrenR <imrenrahbay@gmail.com>
2025-08-13 20:59:17 +0100

    update

--- 10.Day- Sets-Maps/maps.js
+++ 10.Day- Sets-Maps/maps.js
@@ -55,7 +55,7 @@ console.log(countryMap);
 
 const a = [4, 5, 8, 9]
 const b = [3, 4, 5, 7]
-const countries = ['Finland', 'Sweden', 'Norway']
+
 
 //? 1-Find a union b
 
@@ -67,36 +67,4 @@ for ( let i = 0; i < b.length; i++){
 }
 
 console.log(union);
-//? 2-Find a intersection b
-//? 3-Find a with b
-//*****************************  Exercises: Level 3  *******************************************************/
 
-//******************************************************************************************************** */
-
-//? 1-How many languages are there in the countries object file.
-
-//? 2-*** Use the countries data to find the 10 most spoken languages:
-
-   // Your output should look like this
-  //  console.log(mostSpokenLanguages(countries, 10))
-  //  [
-  //    { English: 91 },
-  //    { French: 45 },
-  //    { Arabic: 25 },
-  //    { Spanish: 24 },
-  //    { Russian: 9 },
-  //    { Portuguese: 9 },
-  //    { Dutch: 8 },
-  //    { German: 7 },
-  //    { Chinese: 5 },
-  //    { Swahili: 4 },
-  //    { Serbian: 4 }
-  //  ]
-
-  // Your output should look like this
-  // console.log(mostSpokenLanguages(countries, 3))
-  // [
-  // {English:91},
-  // {French:45},
-  // {Arabic:25}
-  // ]
